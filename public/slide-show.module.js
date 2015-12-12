b71d08c5d39e9bb6811194a349f4ba60dc4f0c08
Ilya Litvinov <ilya.litvinov@dataart.com>
2015-12-12 16:16:54 +0200

    Refactored slideshow

--- public/slide-show.module.js
+++ public/slide-show.module.js
@@ -1,25 +1,54 @@
 /**
  * Created by IlyaLitvinov on 09.12.15.
  */
+
+
 var SlideShow = (function () {
     var duration = 500;
+
     function SlideShow (config) {
-        duration = config.duration;
         this.controls = $(config.root).find('.slideshow_item img');
         this.display = $(config.root).find('.slideshow_display img');
 
         this.init();
     }
+
     SlideShow.prototype.init = function () {
         var self = this;
-        $(this.controls).on('click', function () {
+
+        this.controls.on('click', function () {
             var path = $(this).attr('src');
 
-            $(self.display).fadeOut(duration, function () {
-                $(this).attr('src', path).fadeIn(duration);
+            self.display.fadeOut(duration, function () {
+                $(this)
+                    .attr('src', path)
+                    .fadeIn(duration);
             });
         });
     };
 
     return SlideShow;
 })();
+//var SlideShow = (function () {
+//    var duration = 500;
+//
+//    function SlideShow (config) {
+//        duration = duration || config.duration;
+//        this.controls = $(config.root).find('.slideshow_item img');
+//        this.display = $(config.root).find('.slideshow_display img');
+//
+//        this.init();
+//    }
+//    SlideShow.prototype.init = function () {
+//        var self = this;
+//        $(this.controls).on('click', function () {
+//            var path = $(this).attr('src');
+//
+//            $(self.display).fadeOut(duration, function () {
+//                $(this).attr('src', path).fadeIn(duration);
+//            });
+//        });
+//    };
+//
+//    return SlideShow;
+//})();
