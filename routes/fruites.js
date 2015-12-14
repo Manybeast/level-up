cc184e209d2d8e469b356c572dbfb691960308c1
Ilya Litvinov <ilya.litvinov@dataart.com>
2015-12-14 22:15:03 +0200

    Fixed fruites routes

--- routes/fruites.js
+++ routes/fruites.js
@@ -1,46 +1,42 @@
 /**
  * Created by IlyaLitvinov on 01.12.15.
  */
-var express = require('express'),
-    fruitesModel = require('../models/fruites.js'),
-    router = express.Router();
+const express = require('express');
+const fruitesModel = require('../models/fruites.js');
+const router = express.Router();
 
-
-console.log(fruitesModel.getItem());
 router.get('/', function (req, res) {
-    setTimeout(function () {
-        res.status(200).send(fruites);
-    }, 3000);
+    res.status(200).send(fruitesModel.getItem());
 });
 
 router.post('/', function (req, res) {
     console.log(req.body.fruite);
     if (req.body.fruite) {
-        fruites.push(req.body.fruite);
+        fruitesModel.setItem(req.body.fruite);
         console.log('add to Array');
-        res.status(200).send(fruites);
+        res.status(200).send(fruitesModel.getItem());
     } else {
-        res.status(404).send('not faund');
+        res.status(404).send('not found');
     }
 });
 
 router.put('/:index', function (req, res) {
-    console.log(req.body);
-    if (req.params.index && Number(req.params.index) < fruites.length && req.body.fruite) {
-        fruites.splice(req.params.index, 1, req.body.fruite);
-        console.log('Update in Array');
-        res.status(200).send(fruites);
+    if (req.params.index && Number(req.params.index) < fruitesModel.getItem().length && req.body.fruite) {
+        fruitesModel.updateItem(req.params.index, req.body.fruite);
+
+        res.status(200).send(fruitesModel.getItem());
     } else {
         res.status(500).send('Bad request').end()
     }
 });
 
 router.delete('/:index', function (req, res) {
-    if (req.params.index && Number(req.params.index) < fruites.length) {
+    if (req.params.index && Number(req.params.index) < fruitesModel.getItem().length) {
         setTimeout(function () {
-            fruites.splice(req.params.index, 1);
-            res.status(200).send(fruites);
-        }, 3000);
+
+            fruitesModel.deleteItem(req.params.index, 1);
+            res.status(200).send(fruitesModel.getItem());
+        }, 500);
 
     } else {
         res.status(500).send('Bad request').end()
